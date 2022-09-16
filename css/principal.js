9d40e4686ea0bffcff4d46d0e77dcdc6b8d965fa
vargasyax <yasledyvargasconda@gmail.com>
2022-09-16 07:21:20 -0500

    creando proyecto

---
+++ css/principal.js
@@ -0,0 +1,9 @@
+let navToggle = document.getElementById("nav-toggle")
+
+let navMenu= document.getElementById("nav-menu")
+
+
+navToggle.addEventListener("click",function ()  {  if 
+    (navMenu.classlist.contains(    ".nav-menu"))}
+    {navMenu.classlist.remove(    ".nav-menu"); 
+ }
